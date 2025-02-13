a30294686256b3e0cdb21cbc0f65e2605fcb7b72
nguyen-ngoc-trai <trai.nn.64cntt@ntu.edu.vn>
2025-02-13 16:35:38 +0700

    ví dụ

---
+++ helloworld/src/helloworld/xinchao.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class xinchao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("tôi lần đầu học java");
+	}
+
+}
